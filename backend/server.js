ea259c23da1974b3148308e256c1fa6a4fc2500a
parth354 <parthbajaj05@gmail.com>
2025-09-12 00:13:52 +0530

    Server File Updated for Deployment

--- backend/server.js
+++ backend/server.js
@@ -43,7 +43,7 @@ app.use("/api/comments", commentRoutes);
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'public')));
-  app.get('*', (req, res) => {
+  app.get('/*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
   });
 } else {
